7e0db28f0685dd278eb41c484877d3128a23acae
andrewWong21 <awong21@stuy.edu>
2018-02-02 14:44:34 -0500

    work

---
+++ canvas.js
@@ -0,0 +1,8 @@
+var c = document.getElementByID("draw");
+var ctx = c.getContext();
+var toggleButton = document.getElementByID("toggle");
+var clearButton = document.getElementByID("clear");
+
+var toggle = 
+
+toggleButton.addEventListener("click", toggle)
