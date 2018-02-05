dff576b1709ac3e2c69c0eb651c3e07030c286e1
Andrew <awong21@stuy.edu>
2018-02-04 20:34:38 -0500

    completed

--- canvas.js
+++ canvas.js
@@ -1,8 +1,30 @@
-var c = document.getElementByID("draw");
-var ctx = c.getContext();
-var toggleButton = document.getElementByID("toggle");
-var clearButton = document.getElementByID("clear");
+var canvas = document.getElementById("draw");
+var ctx = canvas.getContext('2d');
+var drawCircles = true;
+var toggleButton = document.getElementById("toggle");
+var clearButton = document.getElementById("clear");
 
-var toggle = 
+var draw = function(e) {
+    ctx.beginPath();
+    if (drawCircles){
+        ctx.arc(event.clientX-7, event.clientY-7, 15, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.fill();
+    }
+    else{
+        ctx.fillRect(event.clientX-22, event.clientY-22, 30, 30);
+    }
+};
 
-toggleButton.addEventListener("click", toggle)
+var toggle = function(e) {
+    drawCircles = !drawCircles;
+};
+
+var clear = function(e) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+ctx.fillStyle = "#87CEEB";
+canvas.addEventListener("click", draw);
+toggleButton.addEventListener("click", toggle);
+clearButton.addEventListener("click", clear);
