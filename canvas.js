f0a881dcc7b9cc4aaa64b7fd9e562be9aed0ff81
Andrew <awong21@stuy.edu>
2018-02-05 22:23:32 -0500

    added connecting lines to circles

--- canvas.js
+++ canvas.js
@@ -3,13 +3,23 @@ var ctx = canvas.getContext('2d');
 var drawCircles = true;
 var toggleButton = document.getElementById("toggle");
 var clearButton = document.getElementById("clear");
+var prevX = -1;
+var prevY = -1;
 
 var draw = function(e) {
-    ctx.beginPath();
     if (drawCircles){
-        ctx.arc(e.offsetX, e.offsetY, 15, 0, 2 * Math.PI);
+        if (prevX != -1){
+            ctx.moveTo(prevX, prevY);
+        };
+        ctx.lineTo(e.offsetX, e.offsetY);
         ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(e.offsetX, e.offsetY, 15, 0, 2 * Math.PI);
         ctx.fill();
+        ctx.stroke();
+        ctx.moveTo(e.offsetX, e.offsetY);
+        prevX = e.offsetX;
+        prevY = e.offsetY;
     }
     else{
         ctx.fillRect(e.offsetX-15, e.offsetY-15, 30, 30);
@@ -22,6 +32,9 @@ var toggle = function(e) {
 
 var clear = function(e) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    prevX = -1;
+    prevY = -1;
 };
 
 ctx.fillStyle = "#87CEEB";
